6cbfb2a6305b5125fc0f6bcdbdfd0375aa80fa76
Himanshu Gupta <59887776+Himanshu-Gupta2001@users.noreply.github.com>
2021-07-30 09:02:24 +0530

    Update spamClassifier.py

--- spamClassifier.py
+++ spamClassifier.py
@@ -1,64 +1,57 @@
-#Naive Bayes
-import os
-import io
-import numpy
-from pandas import DataFrame
+import pandas as pd 
+import numpy as np 
+from nltk.corpus import stopwords
+import string
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression 
+from sklearn.metrics import confusion_matrix 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import pickle
 
-#Function to read files (emails) from the local directory
-def readFiles(path):
-    for root, dirnames, filenames in os.walk(path):
-        for filename in filenames:
-            path = os.path.join(root, filename)
+import warnings 
+warnings.filterwarnings('ignore')
 
-            inBody = False
-            lines = []
-            f = io.open(path, 'r', encoding='latin1')
-            for line in f:
-                if inBody:
-                    lines.append(line)
-                elif line == '\n':
-                    inBody = True
-            f.close()
-            message = '\n'.join(lines)
-            yield path, message
+df = pd.read_csv('spam.csv')
+#Remove duplicates
+df.drop_duplicates(inplace = True)
+
+df = df.sample(frac=1).reset_index(drop=True)
+
+def process_text(text):
+    
+    #1 Remove Punctuationa
+    nopunc = [char for char in text if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    
+    #2 Remove Stop Words
+    clean_words = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+    
+    #3 Return a list of clean words
+    return clean_words
+
+vectorizer = CountVectorizer(analyzer=process_text)
+emails = vectorizer.fit_transform(df['text'])
+X_train, X_test, y_train, y_test = train_test_split(emails, df['spam'], test_size = 0.20, random_state = 0)
+
+models = {'Log':LogisticRegression(), 'Multi':MultinomialNB(),'Forest':RandomForestClassifier(n_estimators=30, criterion='gini', max_features = 10000, random_state =0)}
+
+trained_models = list() 
+
+for model in models.values():
+    model.fit(X_train, y_train)
+    acc = cross_val_score(model, X_train, y_train, scoring='accuracy', cv=10)
+    print(acc.mean())
+    trained_models.append(model)
+
+#Choosing logistic regression after trying it on test data and saving it
+pickle.dump(trained_models, open('trained_model', 'wb'))
 
 
-def dataFrameFromDirectory(path, classification):
-    rows = []
-    index = []
-    for filename, message in readFiles(path):
-        rows.append({'message': message, 'class': classification})
-        index.append(filename)
-
-    return DataFrame(rows, index=index)
-
-#An empty dataframe with 'message' and 'class' headers
-data = DataFrame({'message': [], 'class': []})
-
-#Including the email details with the spam/ham classification in the dataframe
-data = data.append(dataFrameFromDirectory('C:/Users/surya/Desktop/DecemberBreak/emails/spam', 'spam'))
-data = data.append(dataFrameFromDirectory('C:/Users/surya/Desktop/DecemberBreak/emails/ham', 'ham'))
-
-#Head and the Tail of 'data'
-data.head()
-print(data.tail())
-
-vectoriser = CountVectorizer()
-count = vectoriser.fit_transform(data['message'].values)
-print(count)
-
-target = data['class'].values
-print(target)
-
-classifier = MultinomialNB()
-classifier.fit(count, target)
-print(classifier)
-
-exampleInput = ["Hey. This is John Cena. You can't see me", "Free Viagra boys!!", "Please reply to get this offer"]
-excount = vectoriser.transform(exampleInput)
-print(excount)
-
-prediction = classifier.predict(excount)
-print(prediction)
+loaded_model = pickle.load('trained_model', 'rb')
+result = loaded_model.score(X_test, y_test)
+print(result)
